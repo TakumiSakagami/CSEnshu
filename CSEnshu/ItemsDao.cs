80ec44725bbaaa6f0953e185926076e7b4695c10
Administrator <Administrator@WIN-BHSK1S1L9HB>
2018-12-10 16:26:43 +0900

    ItemsDao追加

---
+++ CSEnshu/ItemsDao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ItemsDao
+{
+
+    public List<Items> SearchItemsList()
+    {
+
+        SqlCommand command = new SqlCommand();
+
+
+        //パラメータ設定
+        command.Parameters.Add("@Name", SqlDbType.NVarChar, 50);
+        command.Parameters["@name"].value = itemSerarchBox.Text;
+
+        command.CommandText = $"SELECT * FROM Items " +
+                              $"set Name = @name " +
+                              $"WHERE Name LIKE (N'%{itemSerarchBox}%') ";
+
+        command.Connection = connection;
+        //クエリの実行
+        int num = command.ExecuteNonQuery();
+
+    }
+}
