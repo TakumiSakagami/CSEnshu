124c769b4d07dab3f931d86f6beaad2a55127333
Administrator <Administrator@WIN-6CDKM3KANDQ>
2018-12-10 16:01:21 +0900

    MessageHolderを作成

--- CSEnshu/MessageHolder.cs
+++ CSEnshu/MessageHolder.cs
@@ -8,5 +8,14 @@ namespace CSEnshu
 {
     class MessageHolder
     {
+
+        internal const String EM1 = "半角数字で入力してください";
+        internal const String EM2 = "数字は正の値で入力してください";
+        internal const String EM3 = "在庫が足りません";
+        internal const String EM4 = "商品を選択してください";
+        internal const String EM5 = "顧客を選択してください";
+
+        internal const String PM1 = "注文が完了しました";
+        internal const String PM2 = "在庫の追加が完了しました";
     }
 }
