b079a1aa9a45fbd39f7b427738146bbb2655fef7
Administrator <Administrator@WIN-65HEAO3ENOT>
2018-12-10 16:28:13 +0900

    上野・途中

---
+++ CSEnshu/Properties/OrderDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class OrderDao
+{
+	public OrderDao()
+	{
+        SqlConnection connection = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        command.CommandText = $"UPDATE OrderTable"
+
+        public int OrderRecord(int itemId,int CustomerId,int quantity)
+        {
+        }
+	}
+}
