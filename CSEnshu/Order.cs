9ca3a5aec45025caa67b26363bb1bb4d4e798be9
Administrator <Administrator@WIN-65HEAO3ENOT>
2018-12-11 15:42:41 +0900

    order更新

--- CSEnshu/Order.cs
+++ CSEnshu/Order.cs
@@ -43,7 +43,7 @@ namespace CSEnshu
             foreach (var a in customerDao.SearchCustomerList(customerSearchBox.Text))
             {
                 //customerBoxに表示させる.
-                customerBox.Items.Add($"{a.CustomerId}{a.LastName}{a.FirstName}");
+                customerBox.Items.Add($"顧客ID:{a.CustomerId} 名前:{a.LastName} {a.FirstName}");
             }
 
         }
@@ -72,6 +72,9 @@ namespace CSEnshu
             //②注文数量の変数orderQuantityに入力値を代入
             errorMessage.Text = MessageHolder.PM1;
             orderQuantity = Convert.ToInt32(orderBox.Text);
+
+            logger.
+
         }
 
         //キャンセルボタンを押したらメイン（商品選択画面）に遷移する.
