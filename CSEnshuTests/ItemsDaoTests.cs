063a38d71c157d54b946ef15befd31dc5683b1fc
Administrator <Administrator@WIN-BHSK1S1L9HB>
2018-12-11 15:58:46 +0900

    さーち　追加

---
+++ CSEnshuTests/ItemsDaoTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSEnshu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSEnshu.Tests
+{
+    [TestClass()]
+    public class ItemsDaoTests
+    {
+        [TestMethod()]
+        public void SearchItemsListTest()
+        {
+  
+        }
+    }
+}
