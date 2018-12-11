5587edebe1233a42301b22a698c9e9779f658a0e
Administrator <Administrator@WIN-MIRA9HJRBCA>
2018-12-11 15:42:56 +0900

    tsuno log

--- CSEnshu/Logger.cs
+++ CSEnshu/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,21 @@ namespace CSEnshu
 {
     class Logger
     {
+        private string log;
 
         //log 作成メソッド
         public void WriteLine(List<OrderDto> orderList)
         {
+            var writer = new StreamWriter(@"c:\samples\log.csv");
+            
+            for(int i = 0;i< orderList.Count;i++)
+            {
+
+                log = orderList[i].ItemId + "," + orderList[i].CustomerId + "," + orderList[i].Quantity + "," + orderList[i].Date;
+
+                writer.WriteLine(log);
+
+            }
 
         }
 
