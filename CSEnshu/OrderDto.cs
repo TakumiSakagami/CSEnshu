9bae92792665e29164ec0e6eb9788833da6f0f0c
Administrator <Administrator@WIN-6CDKM3KANDQ>
2018-12-12 13:47:50 +0900

    logでIDじゃなくてNameを表示するように変更

--- CSEnshu/OrderDto.cs
+++ CSEnshu/OrderDto.cs
@@ -10,15 +10,19 @@ namespace CSEnshu
     {
         public int OrderId { get; set; }
         public int ItemId { get; set; }
+        public String ItemName { get; set; }
         public int CustomerId { get; set; }
+        public String CustomerName { get; set; }
         public int Quantity { get; set; }
         public DateTime Date { get; set; }
 
-        public OrderDto(int orderId, int itemId, int customerId, int quantity, DateTime date)
+        public OrderDto(int orderId, int itemId, String itemName, int customerId, String customerName, int quantity, DateTime date)
         {
             this.OrderId = orderId;
             this.ItemId = itemId;
+            this.ItemName = itemName;
             this.CustomerId = customerId;
+            this.CustomerName = customerName;
             this.Quantity = quantity;
             this.Date = date;
         }
